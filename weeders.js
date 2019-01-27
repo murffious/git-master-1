79d53535121a14c0308802d837291813eb928799
Paul Murff <titan.murff@gmail.com>
2019-01-27 16:19:35 -0700

    Create weeders.js

---
+++ weeders.js
@@ -0,0 +1,8 @@
+1. What is callback hell and how to avoid it - look into promises
+2. What is hoisting?
+
+https://medium.com/javascript-in-plain-english/https-medium-com-javascript-in-plain-english-what-is-hoisting-in-javascript-a63c1b2267a1 . 
+
+3. What is a closure?
+4. langauge that gets trasnplied to JS and its benefits
+what does a js framework offer 
