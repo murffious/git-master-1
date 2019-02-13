b15b7b0c1bef6691b263b3ead5bba7c22a716ada
Paul Murff <titan.murff@gmail.com>
2019-02-13 14:42:22 -0700

    Update and rename timerscript.js to teachingaids.js

---
+++ teachingaids.js
@@ -0,0 +1,11 @@
+// build script here for timers 
+1. Click open the lesson plan in the repo and use  jquery to get time values
+2. then auto generate the needed clicks and input to get timers set up based on the plan
+3. push play on 1 then use it on my second laptop to glance over and keep myself on track
+https://www.timeanddate.com/timer/
+
+
+# random student picker 
+http://jsfiddle.net/sMu69/
+
+# obejctives for the day and thumbs 
