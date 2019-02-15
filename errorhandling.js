86dd960880fbc3d9337a77a76db2ce16c5aaddbe
Paul Murff <titan.murff@gmail.com>
2019-02-15 15:18:20 -0700

    Update errorhandling.js

--- errorhandling.js
+++ errorhandling.js
@@ -1,2 +1,4 @@
 ### bootcamp for error handling/reading with node Js and express react and in general 
+
+using the next callback in express with errors 
 https://stackoverflow.com/questions/7310521/node-js-best-practice-exception-handling
