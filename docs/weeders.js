7642574fb2e5c8b6a6585c505a038337562d85c1
Paul Murff <titan.murff@gmail.com>
2019-04-15 10:25:22 -0600

    Update weeders.js

--- docs/weeders.js
+++ docs/weeders.js
@@ -3,8 +3,7 @@
 2. What is [hoisting?] (https://medium.com/javascript-in-plain-english/https-medium-com-javascript-in-plain-english-what-is-hoisting-in-javascript-a63c1b2267a1) 
 
 3. What is a [closure?] (https://medium.com/javascript-scene/master-the-javascript-interview-what-is-a-closure-b2f0d2152b36)
-
-
+Closures are frequently used in JavaScript for object data privacy, in event handlers and callback functions, and in partial applications, currying, and other functional programming patterns.
 
   
 4. Langauge that gets trasnplied to JS and its benefits
