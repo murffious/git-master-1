abf83a2bf834d834f58072d0a6794386e4d03d9f
Paul Murff <titan.murff@gmail.com>
2019-04-15 10:24:11 -0600

    Update weeders.js

--- docs/weeders.js
+++ docs/weeders.js
@@ -1,8 +1,14 @@
-1. What is callback hell and how to avoid it - look into promises
-2. What is hoisting?
+1. What is callback hell and how to avoid it ? - look into promises
 
-https://medium.com/javascript-in-plain-english/https-medium-com-javascript-in-plain-english-what-is-hoisting-in-javascript-a63c1b2267a1 . 
+2. What is [hoisting?] (https://medium.com/javascript-in-plain-english/https-medium-com-javascript-in-plain-english-what-is-hoisting-in-javascript-a63c1b2267a1) 
 
-3. What is a closure?
-4. langauge that gets trasnplied to JS and its benefits
-what does a js framework offer 
+3. What is a [closure?] (https://medium.com/javascript-scene/master-the-javascript-interview-what-is-a-closure-b2f0d2152b36)
+
+
+
+  
+4. Langauge that gets trasnplied to JS and its benefits
+what does a js framework offer ?
+              Typescript 
+              
+5. Accesibility and Mobile Repsonsiveness
